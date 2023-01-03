c3c7d4fcf79705684875228df05f359ace0f4848
Soul <51956374+Soulmeister-x@users.noreply.github.com>
2023-01-03 18:12:35 +0100

    Update SearchResultRepository.kt

--- app/src/main/java/com/example/metbrowser/service/repository/SearchResultRepository.kt
+++ app/src/main/java/com/example/metbrowser/service/repository/SearchResultRepository.kt
@@ -2,7 +2,6 @@ package com.example.metbrowser.service.repository
 
 import com.example.metbrowser.model.SearchResult
 import com.example.metbrowser.model.SearchResultList
-import com.example.metbrowser.service.api.MetBrowserService
 
 /**
  * The goal of this class is to abstract the data source from the rest of the app.
