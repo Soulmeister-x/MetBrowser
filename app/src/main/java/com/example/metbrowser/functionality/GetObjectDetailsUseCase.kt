10dca118adf43ebdd40f88f74eeec17e5cd3b63c
Soul <51956374+Soulmeister-x@users.noreply.github.com>
2023-01-10 16:25:43 +0100

    Switched to Flows completely

---
+++ app/src/main/java/com/example/metbrowser/functionality/GetObjectDetailsUseCase.kt
@@ -0,0 +1,5 @@
+package com.example.metbrowser.functionality
+
+class GetObjectDetailsUseCase {
+    // TODO: implement use case
+}
---
+++ app/src/main/java/com/example/metbrowser/functionality/SearchObjectIdsUseCase.kt
@@ -0,0 +1,5 @@
+package com.example.metbrowser.functionality
+
+class SearchObjectIdsUseCase {
+    // TODO: implement use case
+}
